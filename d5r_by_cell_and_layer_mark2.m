26a5c57b93dab503c1f00c47725b5186e2f57753
adriennemueller <adrienne.l.mueller@gmail.com>
2017-10-02 16:30:02 -0700

    Prettified Figure 3 for D5R Paper.

--- d5r_by_cell_and_layer_mark2.m
+++ d5r_by_cell_and_layer_mark2.m
@@ -28,31 +28,49 @@ function rslt = d5r_by_cell_and_layer_mark2( immuno_struct )
     % Calculate stats compared to (average? across all layers?) values
     Counts_Struct = calc_vs_CTXLayer_pvals( Counts_Struct );
 
+    
+    figure();
+    set(gcf, 'Position', [100, 100, 650, 1000])
+
     % Plot indivdually, for each cell type - 
     for i = 1:length( Counts_Struct )
+        % Skip NeuN Plot
+        if i == 1
+            continue
+        end
         
         Props = Counts_Struct(i).D5R_CT_count ./ Counts_Struct(i).CT_Total_count;
         LayerStrings = {'I', 'II-III', 'IV', 'V', 'VI'};
-
-        figure();
         
-        subplot(1,2,1)
-        barh( (Props .* 100) );
-        title( strcat( Counts_Struct(i).celltype, {' '}, 'vs NeuN' ) );
-        ylabel( LayerStrings(i) );
-        set(gca,'Ydir','reverse')
+        subplot(3,2,i-1)
+        barh( (Props .* 100), 'k' );
+        
+        TickLabel_FontSize = 12; AxisLabel_FontSize = 14;
+        set( gca,'YTickLabel', LayerStrings, 'Ydir','reverse', 'FontSize', TickLabel_FontSize, 'XTick', [0 25 50 75 100], ...
+            'FontWeight', 'Bold' ); xlim( [0 100] );
+        box( gca, 'off');
+        
+        xlabel(  strcat( Counts_Struct(i).celltype, '+' ), 'FontSize', AxisLabel_FontSize, 'FontWeight', 'bold' );
+        if mod(i,2) == 0
+            ylabel( 'Cortical Layer', 'FontSize', AxisLabel_FontSize, 'FontWeight', 'bold' );
+        end
+        
         %%% ADD Counts_Struct(i).VsNeuN_pVals
         
         
-        subplot(1,2,2)
-        barh( (Props( i, : ) .* 100) );
-        title( strcat( Counts_Struct(i).celltype, {' '}, 'vs Layer-Agnostic' ) );
-        ylabel( LayerStrings(i) );
-        set(gca,'Ydir','reverse')
+%         subplot(1,2,2)
+%         barh( (Props( i, : ) .* 100) );
+%         title( strcat( Counts_Struct(i).celltype, {' '}, 'vs Layer-Agnostic' ) );
+%         ylabel( LayerStrings(i) );
+%         set(gca,'Ydir','reverse')
         
         %%% ADD Counts_Struct(i).VsCTSum_pVals = p_val;
         
     end
+    
+    [ax, h1] = suplabel( 'Proportion of Neurons Expressing D5R', 'x' );
+    set(h1, 'FontSize', AxisLabel_FontSize + 2, 'FontWeight', 'Bold' );
+    tightfig( gcf );
 
 end
 
