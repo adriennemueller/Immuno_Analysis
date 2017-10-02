9da478c8e740c70debd1f9537681e8d1c0f464c1
adriennemueller <adrienne.l.mueller@gmail.com>
2017-10-02 16:29:39 -0700

    Prettified Figure 2 for D5R Paper.

--- d5r_plot.m
+++ d5r_plot.m
@@ -4,6 +4,9 @@
 
 function rslt = d5r_plot( immuno_struct )
 
+    TickLabel_FontSize = 14;
+    AxisLabel_FontSize = 18;
+
     % Extract averages for each element across animals
 
     D5R_NeuN  = avg_X_animals( immuno_struct, 'D5R', 'NeuN', 'FEF' );
@@ -17,13 +20,16 @@ function rslt = d5r_plot( immuno_struct )
     
     % Plot
     figure();
+    set(gcf, 'Position', [100, 100, 500, 1000])
+    
     subplot( 2,1,1)
     bar( [D5R_SMI32 D5R_NRG], 'k' );       
     ylim( [0 100] ); set(gca, 'ytick', [0:20:100]);
-    set(gca,'XTickLabel',{'SMI-32','Neurogranin'});
-    ylabel( '% of Neurons with D5R', 'FontSize', 16, 'FontWeight', 'bold' );
+    set(gca,'XTickLabel',{'SMI-32','Neurogranin'}, 'FontSize', TickLabel_FontSize, 'FontWeight', 'bold');
+    ylabel( 'Percentage of Neurons Expressing D5R', 'FontSize', AxisLabel_FontSize, 'FontWeight', 'bold' );
+    xlabel( '', 'FontSize', AxisLabel_FontSize );
+    box(gca,'off');
     set(gca,'XTickLabelRotation',45);
-    set(gca,'FontSize',14, 'FontWeight', 'bold');
     
     D5R_PYR_cell_mat = gen_CMH_cell_mat( immuno_struct, 'D5R', 'PYR' );
     %D5R_PYR_pval = cochran_mantel_haenszel_test( D5R_PYR_cell_mat, 1 )
@@ -34,10 +40,14 @@ function rslt = d5r_plot( immuno_struct )
     subplot( 2,1,2)
     bar( [D5R_Inhib 0 D5R_Parv D5R_Calb D5R_Calr D5R_Som], 'k' );       
     ylim( [0 100] ); set(gca, 'ytick', [0:20:100]);
-    set(gca,'XTickLabel',{'Inhibitory', '', 'Parvalbumin','Calbindin', 'Calretinin', 'Somatostatin'});
-    ylabel( '% of Neurons with D5R', 'FontSize', 16, 'FontWeight', 'bold' );
+    set(gca,'XTickLabel',{'Inhibitory', '', 'Parvalbumin','Calbindin', 'Calretinin', 'Somatostatin'}, ...
+        'FontSize', TickLabel_FontSize, 'FontWeight', 'bold');
+    ylabel( 'Percentage of Neurons Expressing D5R', 'FontSize', AxisLabel_FontSize, 'FontWeight', 'bold' );
+    xlabel( '', 'FontSize', AxisLabel_FontSize );
+    box(gca,'off');
     set(gca,'XTickLabelRotation',45);
-    set(gca,'FontSize',14, 'FontWeight', 'bold');
+    
+
     
     D5R_Inhib1_cell_mat = gen_CMH_cell_mat( immuno_struct, 'D5R', 'ParvCalb' );
     D5R_Inhib1_ChSq_pval = general_chi_sq_test( D5R_Inhib1_cell_mat );
@@ -49,6 +59,8 @@ function rslt = d5r_plot( immuno_struct )
     D5R_Inhib3_ChSq_pval = general_chi_sq_test( D5R_Inhib3_cell_mat );
     
     sigstar( {{'Parvalbumin','Calbindin'}, {'Calbindin', 'Calretinin'}, {'Calretinin', 'Parvalbumin'}}, [D5R_Inhib1_ChSq_pval D5R_Inhib2_ChSq_pval, D5R_Inhib3_ChSq_pval]);
-    
+
+    tightfig( gcf );
+
 end
 
