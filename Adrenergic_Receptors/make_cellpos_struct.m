191b0d51c5d30706d78605164ced828ba3319d56
Adrienne Mueller <adrienne.l.mueller@gmail.com>
2020-01-21 12:29:55 -0800

    Adrenergic Receptor initial plots

---
+++ Adrenergic_Receptors/make_cellpos_struct.m
@@ -0,0 +1,174 @@
+function rslt = make_cellpos_struct()
+
+    rslt = struct;    
+
+    folder = '/Volumes/Hnoss/Papers/Dopamine Receptor Expression/Quantification Images/';
+    % Get a list of all files in the folder with the desired file name pattern.
+    filelist_struct = findfiles( folder, '*.xml' );
+    filelist = {filelist_struct.name};
+    
+    %%% Loop through xml files in folder
+    for i = 1:length(filelist)
+        fname = filelist(i);
+        
+        [pathstr, short_name, ext] = fileparts(char(fname));
+        disp( strcat( 'Processing:', {' '}, short_name ));
+        
+        %%% parse name and append to sruct
+        rslt = parse_fnames(rslt, i, fname);
+        
+        %%% get image xs and ys
+        rslt(i).positions = extract_xy_vals( fname );
+        img_dims = get_img_dimensions( fname );
+        rslt(i).img_x = img_dims(1); rslt(i).img_y = img_dims(2);
+        
+        % add layerdepth info to struct, if exists
+        Ydepths = add_layerdepths( fname );
+        rslt(i).LayerDepths = Ydepths;
+        
+        % add cells for layerdepths
+        CountsByLayer = count_cells_by_layer( rslt(i) );
+        rslt(i).CountsByLayer = CountsByLayer;
+    end
+    
+end
+
+
+function rslt = count_cells_by_layer( substruct )
+    LayerDepths = substruct.LayerDepths;
+    MaxImgDepth = substruct.img_y; % Confirm
+    MaxImgDepth = MaxImgDepth * (512/465);
+
+
+    % If Layerdepths is [], return [];
+    if isempty(LayerDepths)
+        rslt = [];
+        return;
+    end
+    
+    % Get ranges
+    % From LayerDepths - use 5th layerdepth if available, otherwise use end
+    % of img_y.
+    ranges = [ 0              LayerDepths(1); 
+               LayerDepths(1) LayerDepths(2);
+               LayerDepths(2) LayerDepths(3);
+               LayerDepths(3) LayerDepths(4)];
+    if length(LayerDepths) == 5
+        ranges(5,:) = [LayerDepths(4) LayerDepths(5)];
+    else
+        ranges(5,:) = [LayerDepths(4) MaxImgDepth];
+    end
+    
+    
+    % Loop through each range and count cells for R, CT and Both
+    for i = 1:5 % Number of 'layers'
+        rslt(1,i) = length( find( substruct(1).positions(1).y > ranges(i,1)   & ...
+                                  substruct(1).positions(1).y <= ranges(i,2) ) ); % Receptors
+        rslt(2,i) = length( find( substruct(1).positions(2).y > ranges(i,1)  & ...
+                                  substruct(1).positions(2).y <= ranges(i,2) ) ); % Cell Type
+        rslt(3,i) = length( find( substruct(1).positions(3).y > ranges(i,1)  & ...
+                                  substruct(1).positions(3).y <= ranges(i,2) ) ); % Both
+                        
+    end
+    
+end
+
+% add_layerdepths finds a csv file with a similar name as the xmlfile being processed
+% and extracts (returns) the layer depths from that csv file.
+function rslt = add_layerdepths( fullfname )
+    rslt = NaN(2,'double');
+    fullfname = fullfname{1};
+    [folder,fname,ext] = fileparts( fullfname );
+    folder = strcat( folder, '/');
+    
+    strToFind = strcat(fname, '.csv');
+    curr_csv_file = find_file_in_dir( folder, strToFind);
+    if(~isempty(curr_csv_file))
+        rslt = load_layerdepthCSV( strcat(folder, curr_csv_file) );
+    else
+        rslt = [];
+    end
+
+end
+
+
+% parse_fnames loops through a fname and extracts the animal name, region,
+% receptor and cell type from the filename
+function rslt = parse_fnames( rslt, idx, fname )
+    animal_list =  {'Marilyn', 'Bogota', 'Santiago', 'Benny', 'Ozzy', 'BJ28', 'BJ23'};
+    region_list = {'FEF', 'dlPFC'};
+    receptor_list = {'D1R', 'D2R', 'D2Rnew' 'D5R'};
+    celltype_list = {'NeuN', 'Calbindin', 'Calretinin', 'Parvalbumin', 'Somatostatin', 'SMI-32', 'Neurogranin', 'CTB'};     
+
+    rslt(idx).Animal = find_fname_element( animal_list, fname );
+    rslt(idx).Region = find_fname_region( region_list, fname ); %%% FIX FOR DLPFC AND FEF IN SAME NAME
+    rslt(idx).Stain1 = find_fname_element( receptor_list, fname );
+    rslt(idx).Stain2 = find_fname_element( celltype_list, fname );
+    
+    rslt(idx).FileName = fname;
+end
+
+% find_fname_element idenifies whether an element in a list (of e.g.
+% receptor names) is present in a filename
+function element = find_fname_element( list, fname )
+    element = {};
+    
+    fname = fname{1};
+    
+    for i = 1:length(list)
+        rslt = strfind( fname, list{i} );
+        if rslt
+            element = list{i};
+        end
+    end
+    if isempty( element )
+        disp(strcat('Missing element in: ', fname )); 
+    end
+end
+
+% find_fname_region idenifies whether an element in a list region nams is
+% present in a filename; Needs to be different than find_fname_element
+% because the region could be present twice - once to identify the box the
+% secion was taken from and once to idntify the actual region photographed
+function element = find_fname_region( list, fname )
+    fname = fname{1};
+
+    for i = 1:length(list)
+        rslt(i).indexes = strfind( fname, list{i});
+    end
+    
+    curr_max_idx = 0;
+    for i = 1:length(rslt)
+        
+        if isempty(rslt(i).indexes),  continue,  end
+        
+        if rslt(i).indexes(end) > curr_max_idx;
+            curr_max_idx = rslt(i).indexes(end);
+            element = list{i};
+        end
+    end
+    
+    if curr_max_idx == 0;
+        disp(strcat('Missing element in: ', fname )); 
+    end
+    
+end
+
+% get_img_dimensions gets the x and y dimensions for an immuno tif file. 
+% Tif file is assumed to be oriented vertically with superfical cortx at
+% the top and deep cortex at the bottom
+function rslt = get_img_dimensions( fullfname )
+    rslt = NaN(2,'double');
+    fullfname = fullfname{1};
+    [folder,fname,ext] = fileparts( fullfname );
+    folder = strcat( folder, '/');
+    
+    %strToFind = strcat(fname(1:end-4), '.tif');
+    strToFind = strcat(fname, '.tif');
+    curr_im_file = find_file_in_dir( folder, strToFind);
+    if(~isempty(curr_im_file))
+        curr_im = imread(strcat(folder,curr_im_file));
+        [img_y img_x] = size(curr_im);
+        rslt = [img_x img_y];
+    end
+end
---
+++ Adrenergic_Receptors/raw_data_jan20.m
@@ -0,0 +1,157 @@
+
+Ozzy.Alpha1a.NeuN  = [30 423 688];
+Ozzy.Alpha1a.Neurogranin = [606	181	640];
+Ozzy.Alpha1a.RatPyramidal = [340	276	821];
+Ozzy.Alpha1a.SMI32 = [ 653	2	26];
+Ozzy.Alpha1a.Parvalbumin = [ 1816	116	226];
+Ozzy.Alpha1a.Calbindin = [ 435	49	117 ];
+Ozzy.Alpha1a.Calretinin = [ 340	76	186 ];
+Ozzy.Alpha1a.Somatostatin = [694	1	6];
+
+Ozzy.Alpha2a.NeuN = [ 259	228	1351 ];
+Ozzy.Alpha2a.Neurogranin = [489	143	750];
+Ozzy.Alpha2a.RatPyramidal = [940	175	513];
+Ozzy.Alpha2a.SMI32 = [851	2	35];
+Ozzy.Alpha2a.Parvalbumin = [1407	34	73];
+Ozzy.Alpha2a.Calbindin = [666	40	170];
+Ozzy.Alpha2a.Calretinin = [1071	58	116];
+Ozzy.Alpha2a.Somatostatin = [1718	13	250];
+		
+Ozzy.Beta1.NeuN = [481	252	747];
+Ozzy.Beta1.Neurogranin = [427	313	823];
+Ozzy.Beta1.RatPyramidal = [206	400	883];
+Ozzy.Beta1.SMI32 = [600	0	34];
+Ozzy.Beta1.Parvalbumin = [875	111	171];
+Ozzy.Beta1.Calbindin = [811	130	338];
+Ozzy.Beta1.Calretinin = [306	217	34];
+Ozzy.Beta1.Somatostatin = [510	14	22];
+		
+Ozzy.Beta2.NeuN = [152	295	1506];
+Ozzy.Beta2.Neurogranin = [638	161	680];
+Ozzy.Beta2.RatPyramidal = [485	398	1011];
+Ozzy.Beta2.SMI32 = [948	1	45];
+Ozzy.Beta2.Parvalbumin = [828	128	146];
+Ozzy.Beta2.Calbindin = [898	39	296];
+Ozzy.Beta2.Calretinin = [1140	115	394];
+Ozzy.Beta2.Somatostatin = [725	14	175];
+
+
+Quito.Alpha1a.NeuN  = [	241	240	448];
+Quito.Alpha1a.Neurogranin	= [	438	258	639];
+Quito.Alpha1a.RatPyramidal = [];
+Quito.Alpha1a.SMI32 = [661	0 138];
+Quito.Alpha1a.Parvalbumin = [];
+Quito.Alpha1a.Calbindin	= [	760	71	198];
+Quito.Alpha1a.Calretinin = [];
+Quito.Alpha1a.Somatostatin	= [	638	1	9];
+			
+Quito.Alpha2a.NeuN	= [349	57	271];
+Quito.Alpha2a.Neurogranin	= [269	97	463];
+Quito.Alpha2a.RatPyramidal	= [32	462	438];
+Quito.Alpha2a.SMI32	= [1295	6	127];
+Quito.Alpha2a.Parvalbumin = [];
+Quito.Alpha2a.Calbindin = [];
+Quito.Alpha2a.Calretinin = [];
+Quito.Alpha2a.Somatostatin	= [370	157	377];
+			
+Quito.Beta1.NeuN	= [342	365	678];
+Quito.Beta1.Neurogranin	= [481	102	255];
+Quito.Beta1.RatPyramidal	= [67	430	738];
+Quito.Beta1.SMI32	= [350	1	83];
+Quito.Beta1.Parvalbumin	= [538	165	199];
+Quito.Beta1.Calbindin = [];
+Quito.Beta1.Calretinin = [];
+Quito.Beta1.Somatostatin = [];
+
+Quito.Beta2.NeuN	= [325	223	905];
+Quito.Beta2.Neurogranin	= [203	159	561];
+Quito.Beta2.RatPyramidal	= [47	249	592];
+Quito.Beta2.SMI32	= [550	2	138];
+Quito.Beta2.Parvalbumin	= [776	148	248];
+Quito.Beta2.Calbindin = [];
+Quito.Beta2.Calretinin = [];
+Quito.Beta2.Somatostatin = [];
+
+Prince.Alpha1a.NeuN =[	36	559	671];
+Prince.Alpha1a.Neurogranin =[	149	197	290];
+Prince.Alpha1a.RatPyramidal =[	161	342	562];
+Prince.Alpha1a.SMI32	 =[241	14	178];
+Prince.Alpha1a.Parvalbumin =[	461	30	33];
+Prince.Alpha1a.Calbindin	 =[275	31	99];
+Prince.Alpha1a.Calretinin = [];
+Prince.Alpha1a.Somatostatin	 =[414	1	1];
+		
+Prince.Alpha2a.NeuN =[	136	5	579];
+Prince.Alpha2a.Neurogranin =[	307	33	293];
+Prince.Alpha2a.RatPyramidal =[	169	27	537];
+Prince.Alpha2a.SMI32	 =[530	0	23];
+Prince.Alpha2a.Parvalbumin =[	564	16	41];
+Prince.Alpha2a.Calbindin	 =[411	15	72];
+Prince.Alpha2a.Calretinin	 =[503	39	98];
+Prince.Alpha2a.Somatostatin =[	574	0	4];
+				
+Prince.Beta1.NeuN	 =[33	474	437];
+Prince.Beta1.Neurogranin =[	68	76	137];
+Prince.Beta1.RatPyramidal =[	131	158	314];
+Prince.Beta1.SMI32	 =[544	11	131];
+Prince.Beta1.Parvalbumin =[	708	55	47];
+Prince.Beta1.Calbindin	 =[514	69	143];
+Prince.Beta1.Calretinin	 =[377	97	27];
+Prince.Beta1.Somatostatin = [];
+			
+Prince.Beta2.NeuN	 =[16	59	495];
+Prince.Beta2.Neurogranin =[	194	83	355];
+Prince.Beta2.RatPyramidal =[	45	119	545];
+Prince.Beta2.SMI32	 =[442	5	108];
+Prince.Beta2.Parvalbumin =[	452	13	41];
+Prince.Beta2.Calbindin	 =[353	21	89];
+Prince.Beta2.Calretinin	 =[204	26	51];
+Prince.Beta2.Somatostatin	 =[472	0	3];
+
+s.Ozzy = Ozzy;
+s.Quito = Quito;
+s.Prince = Prince;
+
+Animals = {'Ozzy', 'Quito', 'Prince'};
+Receptors = {'Alpha1a', 'Alpha2a', 'Beta1', 'Beta2'};
+NeuronTypes = {'NeuN', 'Neurogranin', 'RatPyramidal', 'SMI32', 'Parvalbumin', 'Calbindin', 'Calretinin', 'Somatostatin'};
+
+neuron_data = struct;
+for k = 1:length( NeuronTypes )
+    
+    receptor_avg = [];
+    for j = 1:length( Receptors )
+        
+        data = [];
+        for i = 1:length( Animals )
+            data = vertcat( data, s.(Animals{i}).(Receptors{j}).(NeuronTypes{k}) );
+        end
+        
+        data_ratios = data(:,3) ./ (data(:,2)+ data(:,3));
+        receptor_avg = horzcat( receptor_avg, mean(data_ratios) );
+        
+    end
+    
+    neuron_data(k).neurontype = NeuronTypes(k);
+    neuron_data(k).vals = receptor_avg;
+    
+end
+
+
+% Plots
+for i = 1:length(neuron_data)
+    
+   figure();
+   set(gcf, 'Position', [100 800 300 250]);
+   bar( neuron_data(i).vals, 'k' )
+   
+   ylim( [0 1] );
+   yticks( [0 0.2 0.4 0.6 0.8 1] );
+   ylabel( {'Proportion of'; 'Receptor+ Neurons'} );
+   
+   xticklabels( {'Alpha1A', 'Alpha2A', 'Beta1', 'Beta2'} );
+   set(gca,'FontSize', 12, 'FontWeight', 'Bold', 'xticklabelrotation', 45 );
+   
+   title( neuron_data(i).neurontype{1}, 'FontSize', 14, 'FontWeight', 'Bold');
+   saveas( gcf, neuron_data(i).neurontype{1}, 'png' );
+end
