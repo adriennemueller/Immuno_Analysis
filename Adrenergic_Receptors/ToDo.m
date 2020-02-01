9490d35fcef453ed556ca4c70d1eeff9aa608594
adriennemueller <adrienne.l.mueller@gmail.com>
2020-01-31 17:07:23 -0800

    Todo list update.

--- Adrenergic_Receptors/ToDo.m
+++ Adrenergic_Receptors/ToDo.m
@@ -3,3 +3,14 @@
 % Qualitative Fig
 
 % Fig legends
+
+- 3 + 3
+- Update figures with that
+- Figure Legends
+- Control Figure
+- Move C into B and remove ns label; and perform tests for other comparisons
+- Table of all chi squ pvalue results and actual proportions and Ns.
+- Remove layer 1 
+- Consider "Number of neurons" vs "Proportion" if not too busy
+
+Also - include somatostatin information in table and in text (inhibitory section).
